cc9c1ed28d57d7e4e8763d0a938e065afc48b0aa
akilaabeysekara <akila2024e@gmail.com>
2026-03-07 01:33:57 +0530

    add multipart resolver bean for form-data support

--- 09_REST_Controller/src/main/java/lk/ijse/config/WebAppConfig.java
+++ 09_REST_Controller/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -1,12 +1,22 @@
 package lk.ijse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.controller")
+@ComponentScan(basePackages = "lk.ijse")
 @EnableWebMvc
 public class WebAppConfig implements WebMvcConfigurer{
+    /*
+     * This bean enables multipart/form-data support.
+     * Without this, Spring MVC cannot read form-data requests.
+     */
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
