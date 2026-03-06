a34df5b8309252849967c516c50c2e88f7d4416a
akilaabeysekara <akila2024e@gmail.com>
2026-03-06 23:25:12 +0530

    update rest controller

--- 09_REST_Controller/src/main/java/lk/ijse/controller/CustomerController.java
+++ 09_REST_Controller/src/main/java/lk/ijse/controller/CustomerController.java
@@ -6,24 +6,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/customer")
 public class CustomerController {
 
-//    @PostMapping("/save")
-//    public String saveCustomer1(){
-//        return "Customer Save 1";
-//    }
-
     //URL embedded - path variable, query params
     //header - key : value
     //body - x-www-form-urlencoded, json, formdata, xml...
 
+    //------------------------------------------------------------------------------------------------------------
 
-    @PostMapping("/save")
-    public String saveCustomer(
-            @RequestParam("name") String name,
+//POST mapping - Save entire customer resource using request parameters
+    @PostMapping("/save1")
+    public String saveCustomerRequestParams(
             @RequestParam("id") String id,
+            @RequestParam("name") String name,
             @RequestParam("address") String address){
-                return "saved-2 " + name + " " + id + " " + address + " ";
+        return "saved-1 " + id + " " + name + " " + address;
     }
 
+    //POST mapping - Save entire customer resource using form data in request body or x-www-form-urlencoded
+    //In modern REST APIs, we normally use JSON instead of form-data
+    @PostMapping("/save2")
+    public String saveCustomerBodyFormData(
+            @RequestParam String id,
+            @RequestParam String name,
+            @RequestParam String address) {
+        return "saved-2 " + id + " " + name + " " + address;
+    }
     //PUT mapping - Update entire customer resource using request parameters
     @PutMapping("/update")
     public String updateCustomer(
